bc31604c686d075920569c0f328fb66efc255ab4
WIN7-20161121ZY\Administrator <gino1993>
2017-01-11 15:58:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/optionmenu/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.administrator.optionmenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu1:
+                
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
